fcac289e18da4d98f1ddac0b71bd7cebac2b6605
Andrea Galliano <andrea.galliano2@studenti.unimi.it>
2023-09-20 12:56:46 +0200

    add Conversazione & Utente classes

---
+++ 6. Pigeongram/Codes/Conversazione.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+/**
+ * OVERVIEW: Conversazione è una classe concreta che rappresenta l'insieme di messaggi scambiati fra 2 utenti.
+ * Una conversazione è caratterizzata da una lista di messaggi che la compongono.
+ * Le istanze di questa classe sono mutabili.
+ */
+
+public class Conversazione {
+    /**
+     * IR: messaggi non deve essere null.
+     * messaggi non deve contenere valori null.
+     * 
+     * AF: messaggi : la lista di messaggi della conversazione.
+     */    
+
+    // ATTRIBUTI
+    /** La lista di messaggi nella conversazione */
+    private final List<Messaggio> messaggi = new ArrayList<Messaggio>();
+
+    // COSTRUTTORE
+
+    /**@throws NullPointerException Se {@code messaggi_in} è null.
+     * Costruisce un nuovo oggetto {@code this} a partire dalla lista di messaggi {@code messaggi_in} fornita per argomento.
+     * 
+     * @param messaggi_in La lista di messaggi con la quale si intende costruire {@code this}.
+     * 
+     * @throws NullPointerException Se {@code messaggi_in} è null.
+     * @throws NullPointerException Se {@code messaggi_in} contiene 1 o più valori null.
+     */
+    public Conversazione(final List<Messaggio> messaggi_in) {
+        Objects.requireNonNull(messaggi_in, "La lista di messaggi non deve essere nulla.");
+
+        for (Messaggio m : messaggi_in) {
+            Objects.requireNonNull(m, "La lista di messaggi non deve contenere valori null.");
+            
+            messaggi.add(m);
+        }
+    }
+
+    // METODI
+
+    /**
+     * Restituisce i messaggi di {@code this}.
+     * 
+     * @return La lista immutabile di messaggi di {@code this}.
+     */
+    public List<Messaggio> getMessaggi() {
+        return Collections.unmodifiableList(messaggi);
+    }
+}
---
+++ 6. Pigeongram/Codes/Utente.java
@@ -0,0 +1,163 @@
+import java.util.*;
+
+/**
+ * OVERVIEW: Utente è una classe concreta atta a rappresentare stato e metodi di un Utente che intende scambiare messaggi.
+ * Un Utente è caratterizzato dal suo nome e dalla sua password.
+ * Le istanze di questa classe sono mutabili.
+ */
+
+public class Utente {
+    /**
+     * IR: username non deve essere null.
+     * username non deve essere vuoto.
+     * username non deve contenere spazi.
+     * La password non deve essere null.
+     * La password non deve essere vuota.
+     * La lista di conversazioni non deve essere null.
+     * La lista di conversazioni non deve contenere valori null.
+     * 
+     * AF: username : il nome utente
+     * password : la password dell'utente
+     * conversazioni : le conversazioni dell'utente
+     */
+
+    // ATTRIBUTI
+    /** username dell'utente */
+    private final String username;
+    /** La password dell'utente, che all'occorrenza può essere modificata, ma mai visualizzata in chiaro */
+    private String password;
+    /** La lista di conversazioni dell'utente */
+    private List<Conversazione> conversazioni = new ArrayList<Conversazione>();
+
+    // COSTRUTTORE
+
+    /**
+     * Costruisce un nuovo oggetto {@code this} a partire da {@code username}, la {@code password} e la lista di conversazioni {@code conversazioni} avuti per argomento.
+     * 
+     * @param username Lo username di {@code this}.
+     * @param password La password di {@code this}.
+     * @param conversazioni La lista di conversazioni di {@code this}.
+     * 
+     * @throws NullPointerException se {@code username} è null.
+     * @throws NullPointerException se {@code password} è null.
+     * @throws NullPointerException se {@code conversazioni} è null.
+     * @throws NullPointerException se {@code conversazioni} contiene valori null.
+     * @throws IllegalArgumentException se {@code username} è vuoto.
+     * @throws IllegalArgumentException se {@code username} contiene spazi.
+     * @throws IllegalArgumentException se {@code password} è vuota.
+     */
+    public Utente(final String username, final String password, final List<Conversazione> conversazioni_in) {
+        Objects.requireNonNull(username, "username non deve essere null.");
+        Objects.requireNonNull(password, "username non deve essere null.");
+        Objects.requireNonNull(conversazioni_in, "la lista di conversazioni non deve essere null.");
+
+        if (username.isEmpty()) throw new IllegalArgumentException("username non deve essere vuoto.");
+
+        if (password.isEmpty()) throw new IllegalArgumentException("La password non deve essere vuota.");
+
+        if (username.contains(" ")) throw new IllegalArgumentException("username non deve contenere spazi.");
+
+        for (Conversazione c : conversazioni_in) {
+            Objects.requireNonNull(c, "La lista di conversazioni non deve contenere valori null.");
+
+            conversazioni.add(c);
+        }
+
+        this.username = username;
+        this.password = password;
+    }
+
+    // METODI
+
+    /**
+     * Restituisce lo username di {@code this}.
+     * 
+     * @return Lo username di {@code this}.
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Imposta una nuova password {@code password} avuta per argomento per l'Utente {@code this}.
+     * 
+     * @param password La nuova passowrd che si intende impostare per {@code this}.
+     * 
+     * @throws NullPointerException nel caso in cui {@code password} fosse nulla.
+     * @throws IllegalArgumentException nel caso in cui {@code password} fosse vuota.
+    */
+    public void setPassword(String password) {
+        Objects.requireNonNull(password, "La password avuta per argomento non deve essere nulla.");
+
+        if (password.isEmpty()) throw new IllegalArgumentException("La password avuta per argomento non deve essere vuota.");
+
+        this.password = password;
+    }
+
+
+    /**
+     * Restituisce un valore corrispondente al risultato dell'autenticazione di {@code this} grazie a {@code username} e {@code password} avuti per argomento.
+     * La codifica del risultato è la seguente: <br/><br/>
+     * 1 --> autenticato con successo <br/><br/>
+     * 0 --> non autenticato 
+     * 
+     * @param username Lo username con cui si intende effettuare l'autenticazione con {@code this}.
+     * @param password La password con cui si intende effettuare l'autenticazione con {@code this}.
+     * 
+     * @return Il risultato dell'autenticazione di {@code this}.
+     * 
+     * @throws NullPointerException se {@code username} è null.
+     * @throws NullPointerException se {@code password} è null.
+     * @throws IllegalArgumentException se {@code username} è vuoto.
+     * @throws IllegalArgumentException se {@code username} contiene spazi.
+     * @throws IllegalArgumentException se {@code password} è vuota.
+     */
+    public int autenticazione(final String username, final String password) {
+        Objects.requireNonNull(username, "Lo username non deve essere vuoto.");
+        Objects.requireNonNull(password, "Lo username non deve essere vuoto.");
+
+        if (username.isEmpty()) throw new IllegalArgumentException("Lo username non deve essere vuoto.");
+
+        if (password.isEmpty()) throw new IllegalArgumentException("La password non deve essere vuota.");
+
+        if (username.contains(" ")) throw new IllegalArgumentException("Lo username non deve contenere spazi.");
+
+        if (this.username == username && this.password == password) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.username, this.password);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Utente other = (Utente) obj;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return username;
+    }
+}
