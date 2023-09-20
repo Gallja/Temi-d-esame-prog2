4b763634ec7387f5343b383729720f4b62b9f7b5
Andrea Galliano <andrea.galliano2@studenti.unimi.it>
2023-09-20 10:23:01 +0200

    fix Testo class

--- 6. Pigeongram/Codes/Testo.java
+++ 6. Pigeongram/Codes/Testo.java
@@ -6,14 +6,16 @@
 
 public class Testo extends Contenuto {
     
+    // COSTRUTTORE
+
     /**
      * Costruisce un nuovo oggetto {@code this} a partire dall'identificativo {@code id}
-     * @param id
-     * @param arg
+     * 
+     * @param id Il carattere identificativo del Testo.
+     * @param arg L'argomento del Testo.
      */
     public Testo(final char id, final String arg) {
         super(id, arg);
     }
-
 
 }
